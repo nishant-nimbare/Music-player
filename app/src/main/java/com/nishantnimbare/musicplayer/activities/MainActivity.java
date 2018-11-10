ebfd011987787a370cef9bf6b392243892c64ee3
nishant nimbare <nishantnimbare@gmail.com>
2018-11-10 19:04:55 +0530

    binded service

--- app/src/main/java/com/nishantnimbare/musicplayer/activities/MainActivity.java
+++ app/src/main/java/com/nishantnimbare/musicplayer/activities/MainActivity.java
@@ -1,13 +1,18 @@
 package com.nishantnimbare.musicplayer.activities;
 
+import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.IBinder;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -15,6 +20,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,15 +30,24 @@ import com.nishantnimbare.musicplayer.model.Song;
 import com.nishantnimbare.musicplayer.service.mService;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     ArrayList<Song> songs;
-    Button button;
     boolean isPlaying=false;
     RecyclerView.Adapter adapter;
     RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+
+    mService service;
+    private ServiceConnection serviceConn;
+
+    Intent serviceIntent;
+
+    int songPos=-1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,47 +55,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         songs = new ArrayList<>();
-        button=(Button)findViewById(R.id.start);
 
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Log.e(TAG, "onClick: button clicked");
-
-                    if(isPlaying) {
-
-                        Intent intent = new Intent(MainActivity.this,mService.class);
-                        intent.setAction(mService.ACTION_PLAY);
-                        startService(intent);
-                        isPlaying=false;
-                        button.setText("PLAY");
-
-                    }else {
-
-                        Intent intent = new Intent(MainActivity.this,mService.class);
-                        intent.setAction(mService.ACTION_PLAY);
-                        startService(intent);
-                        isPlaying=true;
-                        button.setText("PAUSE");
-                    }
-
-            }
-        });
 
         recyclerView=(RecyclerView)findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                layoutManager.getOrientation());
+
+
+        recyclerView.addItemDecoration(dividerItemDecoration);
         adapter = new myAdapter();
         recyclerView.setAdapter(adapter);
 
+
+        serviceIntent =new Intent(MainActivity.this,mService.class);
+
+        startService(serviceIntent);
+
+        //binding the service
+        serviceConn = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder binder) {
+
+                mService.myServiceBinder mBinder = (mService.myServiceBinder)binder;
+                 service =(mService)mBinder.getService();
+
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        };
+
+        bindService(serviceIntent,serviceConn,Context.BIND_AUTO_CREATE);
+
         getMp3Songs();
+
+
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        stopService(new Intent(MainActivity.this,mService.class));
+        unbindService(serviceConn);
+        stopService(serviceIntent);
     }
 
 
@@ -125,10 +148,49 @@ public class MainActivity extends AppCompatActivity {
             cursor.close();
             /*db.closeDatabase();*/
         }
+
+        Collections.sort(songs,new mComparator());
         adapter.notifyDataSetChanged();
     }
 
+    public void playPrev(View view) {
 
+        if(songPos==0){
+            songPos=songs.size()-1;
+        }else{
+            songPos--;
+        }
+
+        service.playSong(songs.get(songPos).getPath());
+        isPlaying=true;
+        findViewById(R.id.btnplay).setBackgroundResource(R.drawable.pause);
+    }
+
+    public void playNext(View view) {
+
+        songPos= (songPos+1)%songs.size();
+        service.playSong(songs.get(songPos).getPath());
+        isPlaying=true;
+        findViewById(R.id.btnplay).setBackgroundResource(R.drawable.pause);
+    }
+
+    public void playClicked(View view) {
+        if(isPlaying) {
+            isPlaying = false;
+            findViewById(R.id.btnplay).setBackgroundResource(R.drawable.play);
+        }else{
+            isPlaying=true;
+            findViewById(R.id.btnplay).setBackgroundResource(R.drawable.pause);
+        }
+
+        if(songPos==-1){
+            songPos=0;
+            service.playSong(songs.get(songPos).getPath());
+
+        }else {
+            service.playClicked();
+        }
+    }
 
     //Recycler view adapter here
 
@@ -166,15 +228,24 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         Toast.makeText(MainActivity.this, " "+songItem.getText(), Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(MainActivity.this,mService.class);
-                        intent.putExtra("song_path",songs.get(getAdapterPosition()).getPath());
-                        intent.setAction(mService.ACTION_START);
-                        startService(intent);
+                      songPos = getAdapterPosition();
+                        service.playSong(songs.get(songPos).getPath());
                         isPlaying=true;
-                        button.setVisibility(View.VISIBLE);
+                        findViewById(R.id.btnplay).setBackgroundResource(R.drawable.pause);
                     }
                 });
             }
         }
     }
+
+
+    public class mComparator implements Comparator<Song> {
+
+        @Override
+        public int compare(Song s1, Song s2) {
+            return s1.getName().compareTo(s2.getName());
+        }
+
+    }
+
 }
--- app/src/main/java/com/nishantnimbare/musicplayer/service/mService.java
+++ app/src/main/java/com/nishantnimbare/musicplayer/service/mService.java
@@ -4,77 +4,93 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.MediaController;
 
 import java.io.IOException;
 
 public class mService extends Service {
 
     private static final String TAG = "mService";
-
+/*
     public static final String ACTION_START ="com.nishantnimbare.musicplayer.action.start";
     public static final String ACTION_STOP ="com.nishantnimbare.musicplayer.action.stop";
     public static final String ACTION_PLAY ="com.nishantnimbare.musicplayer.action.play";
     public static final String ACTION_PREV ="com.nishantnimbare.musicplayer.action.prev";
-    public static final String ACTION_NEXT ="com.nishantnimbare.musicplayer.action.next";
+    public static final String ACTION_NEXT ="com.nishantnimbare.musicplayer.action.next";*/
 //    public static final int NOTI_ID =74216;
 
 
 
     MediaPlayer mediaPlayer;
 
+
+
+    public class myServiceBinder extends Binder{
+        public mService getService(){
+            return mService.this;
+        }
+    }
+
+    private IBinder myBinder = new myServiceBinder();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Log.e(TAG, "onBind: service bound");
+        return myBinder;
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        String song_path=intent.getStringExtra("song_path");
-
-
-        if(intent.getAction().equals(ACTION_START)) {
-            mediaPlayer = new MediaPlayer();
-
-            try {
-                mediaPlayer.setDataSource(getApplicationContext(), Uri.parse(song_path));
-                Log.e(TAG, "onClick: dataSource set");
-                Log.e(TAG, "path "+song_path);
-                mediaPlayer.prepareAsync();
-                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mediaPlayer) {
-                        mediaPlayer.start();
-                        Log.e(TAG, "onPrepared: mediaPlayer started");
-
-                    }
-                });
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-        }else if(intent.getAction().equals(ACTION_PLAY)){
-
-            Log.e(TAG, "onStartCommand: play clicked");
-            if(mediaPlayer.isPlaying()){
-                mediaPlayer.pause();
-                Log.e(TAG, "paused!!");
-            }else{
-                mediaPlayer.start();
-                Log.e(TAG, "played!!");
-            }
-
-        }else {
-            Log.e(TAG, "onStartCommand: action "+intent.getAction());
-        }
+        mediaPlayer = new MediaPlayer();
+        Log.e(TAG, "onStartCommand: sevice started");
 
         return START_STICKY;
     }
+
+
+   public void playSong(String path){
+
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+        }
+
+        try {
+
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(getApplicationContext(), Uri.parse(path));
+            Log.e(TAG, "onClick: dataSource set");
+            Log.e(TAG, "path "+path);
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mediaPlayer.start();
+                    Log.e(TAG, "onPrepared: mediaPlayer started");
+
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void playClicked() {
+
+    if(mediaPlayer.isPlaying()){
+        mediaPlayer.pause();
+    }else{
+        mediaPlayer.start();
+    }
+
+    }
+
 
     @Override
     public void onDestroy() {
